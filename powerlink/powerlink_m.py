48f5f822bf0d7baa2f7cda4f9cf987d14c4f4252
Lena <78489561+yena-lena@users.noreply.github.com>
2022-02-10 18:33:00 +0900

    .

---
+++ powerlink/powerlink_m.py
@@ -0,0 +1,49 @@
+import csv
+import requests
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+#cp949 에러 발생시 csv파일 인코딩을 메모장에서 ANSI로 변경
+## file_name : 파워링크 검색 키워드 목록
+## find : 탐색할 업체
+file_name = "keyword.csv"
+find = "에듀윌"
+
+f = open(file_name,'r', encoding="utf-8")
+df = pd.read_csv(f)
+keywords = df['이름']
+
+m_data = []
+m_url = 'https://m.ad.search.naver.com/search.naver?sm=&where=m_expd&query='
+
+for keyword in keywords:
+    m_base_url = m_url+quote_plus(keyword)
+
+    response = requests.get(m_base_url)
+
+    if response.status_code == 200:
+      soup = BeautifulSoup(response.content, 'html.parser')
+
+    items = soup.find_all(attrs={'class':'tit'})
+
+    rank = 1
+    temp = 0
+
+    for item in items[:10]:#10위까지만 탐색
+      serial = item.get_text()
+      #print(serial[:10])
+      if find in serial:
+          temp = rank
+          #print(temp)
+      rank = rank+1
+    m_data.append([keyword,temp])
+
+# 결과 저장
+result = pd.DataFrame(m_data, columns=['Name','Rank'])
+print(result)
+result.to_csv('result_m.csv', encoding='euc-kr')
+
+
